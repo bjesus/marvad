e3b8e3b583c58266ac4bdc343e5ded2eaa58b280
Yo'av Moshe <bjesus@localhost.localdomain>
2012-08-24 18:07:27 +0300

    name & phone validators

--- Item.js
+++ Item.js
@@ -4,8 +4,8 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Item', {
     content: { type: Sequelize.TEXT, validate: { notNull: true } },
     kind: { type: Sequelize.STRING, defaultValue: "ride", validate: { isAlpha: true, notNull: true } },
-    phone: { type: Sequelize.STRING, validate: { isNumeric: true, len: 10, notNull: true } },
-    name: { type: Sequelize.STRING, validate: { notNull: true } },
+    phone: { type: Sequelize.STRING, validate: { isNumeric: true, notNull: true,  isTen: function(value) { if(value.length !== 10) { throw new Error('Has to be exactly 10 digits!') } } } },
+    name: { type: Sequelize.STRING, validate: { notNull: true, hasSpace: function(value) { if(value.search(' ') < 0) { throw new Error('Has to have space!') } } } },
     area: { type: Sequelize.STRING },
     time: { type: Sequelize.DATE, validate: { notNull: true } },
     })
